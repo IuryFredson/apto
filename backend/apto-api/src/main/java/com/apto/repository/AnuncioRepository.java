faccddf33237faa7e52cebc169118dc412503937
geugenio <geugnio12@gmail.com>
2026-04-01 23:19:48 -0300

    feat: adicionar AnuncioRepository e MoradiaRepository para persistencia dos dados

---
+++ backend/apto-api/src/main/java/com/apto/repository/AnuncioRepository.java
@@ -0,0 +1,14 @@
+package com.apto.repository;
+
+import com.apto.model.entity.Anuncio;
+import com.apto.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AnuncioRepository extends JpaRepository<Anuncio, UUID> {
+    Optional<Anuncio> getAnuncioByAnunciante_Id(UUID usuarioId);
+}
