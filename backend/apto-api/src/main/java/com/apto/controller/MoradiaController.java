1b5aa8178c0a3af185bb81db0cc3a18580972243
geugenio <geugnio12@gmail.com>
2026-04-02 17:15:19 -0300

    feat: adicionar Controller para Moradia

---
+++ backend/apto-api/src/main/java/com/apto/controller/MoradiaController.java
@@ -0,0 +1,43 @@
+package com.apto.controller;
+
+import com.apto.dto.request.AtualizarMoradiaRequestDTO;
+import com.apto.dto.request.CriarMoradiaRequestDTO;
+import com.apto.dto.response.MoradiaResponseDTO;
+import com.apto.service.MoradiaService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/moradias")
+public class MoradiaController {
+    private final MoradiaService moradiaService;
+
+    public MoradiaController(MoradiaService moradiaService) {
+        this.moradiaService = moradiaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MoradiaResponseDTO>> listarTodas(){
+        return ResponseEntity.ok(moradiaService.listarTodos());
+    }
+
+    @PostMapping
+    public ResponseEntity<MoradiaResponseDTO> criar(@Valid @RequestBody CriarMoradiaRequestDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(moradiaService.criar(dto));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<MoradiaResponseDTO> atualizr(@PathVariable UUID id, @Valid @RequestBody AtualizarMoradiaRequestDTO dto){
+        return ResponseEntity.ok(moradiaService.atualizar(id,dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MoradiaResponseDTO> excluir(@PathVariable UUID id){
+        moradiaService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
