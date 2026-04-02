8d900a603c6ea0bea8734a6f6a4697266fb73682
geugenio <geugnio12@gmail.com>
2026-04-01 23:02:48 -0300

    feat: adicionar exceções de AnuncioNaoEncontradoException e MoradiaNaoEncontradaException

---
+++ backend/apto-api/src/main/java/com/apto/exception/AnuncioNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.apto.exception;
+
+public class AnuncioNaoEncontradoException extends RuntimeException {
+    public AnuncioNaoEncontradoException(String message) {
+        super(message);
+    }
+}
