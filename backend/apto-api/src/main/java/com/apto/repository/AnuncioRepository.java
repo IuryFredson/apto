e9148a0d27e402f7ed4da3fca681cf6b0c0e5df2
geugenio <geugnio12@gmail.com>
2026-04-02 18:14:19 -0300

    feat: adicionar no repository método boolean para existsByMoradia

--- backend/apto-api/src/main/java/com/apto/repository/AnuncioRepository.java
+++ backend/apto-api/src/main/java/com/apto/repository/AnuncioRepository.java
@@ -1,6 +1,7 @@
 package com.apto.repository;
 
 import com.apto.model.entity.Anuncio;
+import com.apto.model.entity.Moradia;
 import com.apto.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,5 @@ import java.util.UUID;
 @Repository
 public interface AnuncioRepository extends JpaRepository<Anuncio, UUID> {
     Optional<Anuncio> getAnuncioByAnunciante_Id(UUID usuarioId);
+    Boolean existsByMoradia(Moradia moradia);
 }
