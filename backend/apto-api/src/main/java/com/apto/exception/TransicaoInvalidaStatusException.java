9e7a5474be198c2c0d5c4452e275a01a4fdeb3a3
geugenio <geugnio12@gmail.com>
2026-04-21 02:34:13 -0300

    feat: adicionar exceção para Transição Invalida

---
+++ backend/apto-api/src/main/java/com/apto/exception/TransicaoInvalidaStatusException.java
@@ -0,0 +1,7 @@
+package com.apto.exception;
+
+public class TransicaoInvalidaStatusException extends RuntimeException {
+    public TransicaoInvalidaStatusException(String message) {
+        super(message);
+    }
+}
